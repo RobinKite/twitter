9034b2fd1166860f5b10fe50b9859dba60f61a59
kolya2022 <fox20o1234@gmail.com>
2023-09-23 20:08:31 +0300

    base configure

--- twitter/src/App.js
+++ twitter/src/App.js
@@ -1,12 +1,7 @@
 import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <div>skdsd</div>
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
-C;
