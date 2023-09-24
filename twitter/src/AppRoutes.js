cc714a0ed6fcd48e5298148bab1b04056f0a63d6
Alina Motrii <alinamotrii92@gmail.com>
2023-09-24 19:27:48 -0230

    in process

---
+++ twitter/src/AppRoutes.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+
+function AppRoutes() {
+  return (
+    <Routes>
+      <Route exact path="/" element={<div>Home</div>} />
+      <Route path="/explore" element={<div>Explore</div>} />
+      <Route path="/notifications" element={<div>Notifications</div>} />
+      <Route path="/messages" element={<div>Messages</div>} />
+      <Route path="/lists" element={<div>Lists</div>} />
+      <Route path="/communities" element={<div>Communities</div>} />
+      <Route path="/verified" element={<div>Verified</div>} />
+      <Route path="/profile" element={<div>Profile</div>} />
+      <Route path="/more" element={<div>More</div>} />
+    </Routes>
+  );
+}
+export default AppRoutes;
---
+++ twitter/src/Sidebar/Sidebar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import SidebarItem from "../SidebarItem/SidebarItem";
+import Button from "../Button/Button";
+const Sidebar = () => {
+  return (
+    <div>
+      <SidebarItem />
+      <Button>Post</Button>
+    </div>
+  );
+};
+export default Sidebar;
