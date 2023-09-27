81b435a6fc0ca17ad6ebd6241567a02a08b3c7fc
Alina Motrii <alinamotrii92@gmail.com>
2023-09-27 13:27:41 -0230

    button styles added

--- src/Components/Sidebar/Sidebar.jsx
+++ src/Components/Sidebar/Sidebar.jsx
@@ -1,11 +1,19 @@
 import React from "react";
 import SidebarItem from "../SidebarItem/SidebarItem";
-import Button from "../Button/Button";
+
+import Button from "@mui/material/Button";
 const Sidebar = () => {
   return (
     <div>
       <SidebarItem />
-      <Button>Post</Button>
+      <Button variant="contained" sx={{
+        borderRadius: "20px",
+        padding: '10px 70px',
+        color: 'rgb(255, 255, 255)',
+        fontWeight: '700',
+             fontSize: '25px',
+
+      }}>Post</Button>
     </div>
   );
 };
