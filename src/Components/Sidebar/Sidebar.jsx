3f6bb4a13aa19f559adb87866d631e5783de3f1e
Alina Motrii <alinamotrii92@gmail.com>
2023-10-12 11:04:41 -0230

    dropdown menu added

--- src/Components/Sidebar/Sidebar.jsx
+++ src/Components/Sidebar/Sidebar.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import SidebarItem from "../SidebarItem/SidebarItem";
 import Button from '../Button/Button'
-// import Button from "@mui/material/Button";
+
 const Sidebar = () => {
   return (
     <div>
