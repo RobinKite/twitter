42d502f5c6cd91e553a61054fba216220cb69833
Ruslan Selykh <rusyselih@gmail.com>
2023-11-18 01:01:44 +0200

    fix conflikt

--- src/Components/Sidebar/Sidebar.jsx
+++ src/Components/Sidebar/Sidebar.jsx
@@ -40,8 +40,6 @@ const Sidebar = () => {
   );
   const dispatch = useDispatch();
 
-
-
   const isMobile = useMediaQuery("(max-width: 1280px)");
   const [anchorEl, setAnchorEl] = React.useState(null);
   const open = Boolean(anchorEl);
