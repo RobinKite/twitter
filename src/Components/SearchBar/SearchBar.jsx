d90fe7d0de8cf2570f9ead540c20d7b53415fd75
Anastasia Aleksandrova <anastasiayurievnaaleksandrova@gmail.com>
2023-11-21 02:26:02 +0200

    fix position&style icons&clear btn for searchbar

--- src/Components/SearchBar/SearchBar.jsx
+++ src/Components/SearchBar/SearchBar.jsx
@@ -1,39 +1,42 @@
 import React, { useState } from "react";
 import SearchIcon from "@mui/icons-material/Search";
-import { Stack, TextField } from "@mui/material";
-import { searchBarSX } from "./styleSX";
+import CancelIcon from "@mui/icons-material/Cancel";
+import { Input, Stack } from "@mui/material";
+import { closeIconSX, inputSX, searchBarSX, searchIconSX } from "./styleSX";
 
 const SearchBar = () => {
   const [searchTerm, setSearchTerm] = useState("");
 
   const handleSearch = (e) => {
     e.preventDefault();
-    // Зробити fetch
+    // TODO: add fetch
     console.log(searchTerm);
   };
 
-  const inputStyle = {
-    height: "15px",
+  const handleKeyDown = (event) => {
+    if (event.key === "Enter" && searchTerm.trim() !== "") {
+      handleSearch();
+    }
+  };
+
+  const handleInputClear = () => {
+    setSearchTerm("");
   };
 
   return (
     <Stack sx={searchBarSX}>
-      <form onSubmit={handleSearch}>
-        <TextField
-          variant="outlined"
-          fullWidth
-          placeholder="Search"
-          value={searchTerm}
-          onChange={(e) => setSearchTerm(e.target.value)}
-          InputProps={{
-            startAdornment: (
-              <SearchIcon style={{ marginLeft: "8px", marginRight: "8px" }} />
-            ),
-            style: { borderRadius: "25px" },
-          }}
-          inputProps={{ style: inputStyle }}
-        />
-      </form>
+      <Input
+        sx={inputSX}
+        placeholder="Search"
+        disableUnderline
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+        onKeyDown={handleKeyDown}
+      />
+      <SearchIcon sx={searchIconSX} />
+      {searchTerm.trim() && (
+        <CancelIcon sx={closeIconSX} onClick={handleInputClear} />
+      )}
     </Stack>
   );
 };
