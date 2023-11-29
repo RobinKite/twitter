40295dc29d4b77de6a9c9c8cbc17b9029c73c958
Tetiana Ponomarova <tadimm0@gmail.com>
2023-11-29 16:37:44 +0200

    changed width for Button

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -3,7 +3,7 @@ import { styled } from "@mui/material/styles";
 
 export const Button = styled(MuiButton)(({ theme }) => ({
   height: "40px",
-  width: "300px",
+  minWidth: "300px",
   // margin: 5,
   border: "0.6px solid #cfd9de",
   borderRadius: 150,
