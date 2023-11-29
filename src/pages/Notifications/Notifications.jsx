18017ecd9fdb6043f689a5c3fbc00b0ef47c6d97
Maksym Chyzhevskyi <mchyzhevsky@gmail.com>
2023-11-29 18:39:27 +0200

    change to MUI

--- src/pages/Notifications/Notifications.jsx
+++ src/pages/Notifications/Notifications.jsx
@@ -1,6 +1,7 @@
 import TabPanel from "@mui/lab/TabPanel";
 import { ProfileTabs, AllNotifications } from "../../components";
-import styles from "./Notifications.module.scss";
+import { Stack, Typography } from "@mui/material";
+import { title } from "./styleSX";
 
 const tabs = [
   { label: "All", value: "0" },
@@ -10,8 +11,10 @@ const tabs = [
 
 export const Notifications = () => {
   return (
-    <div>
-      <h2 className={styles.title}>Notifications</h2>
+    <Stack>
+      <Typography variant="h2" sx={title}>
+        Notifications
+      </Typography>
       <ProfileTabs
         tabs={tabs}
         variant="scrollable"
@@ -27,6 +30,6 @@ export const Notifications = () => {
         <TabPanel value="1">Verified</TabPanel>
         <TabPanel value="2">Mentions</TabPanel>
       </ProfileTabs>
-    </div>
+    </Stack>
   );
 };
---
+++ src/pages/Notifications/styleSX.jsx
@@ -0,0 +1,7 @@
+export const title = {
+  fontSize: "20px",
+  fontWeight: "bold",
+  padddingRight: "16px",
+  padddingLeft: "16px",
+  paddingTop: "16px",
+};
